62ede7b3a95c1d965730c49867f3339f094f9535
natsusao <natsusao@gmail.com>
2021-03-20 16:14:17 +0800

    add yaml-cpp

--- tests/test_config.cc
+++ tests/test_config.cc
@@ -1,11 +1,55 @@
-#include "../sylar/config.h"
-#include "../sylar/log.h"
+#include "sylar/config.h"
+#include "sylar/log.h"
+#include <yaml-cpp/yaml.h>
 
 sylar::ConfigVar<int>::ptr g_int_value_config = 
     sylar::Config::Lookup("system.port", (int)8080, "system port");
 
+void print_yaml(const YAML::Node& node, int level)
+{
+    if(node.IsScalar())
+    {
+        SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ')
+            << node.Scalar() << " - " << node.Type() << " - " << level;
+    }
+    else if(node.IsNull())
+    {
+        SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ')
+            << "NULL - " << node.Type() << " - " << level;
+    }
+    else if(node.IsMap())
+    {
+        for(auto it = node.begin();
+            it != node.end(); ++ it)
+        {
+            SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ')
+                << it->first << " - " << it->second.Type() << " - " << level;
+            print_yaml(it->second, level + 1);
+        }
+    }
+    else if(node.IsSequence()) 
+    {
+        for(size_t i = 0; i< node.size(); ++ i)
+        {
+            SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ')
+                << i << " - " << node[i].Type() << " - " << level;
+            print_yaml(node[i], level + 1);
+        }
+    }
+}
+
+void test_yaml() {
+    //加载进来源文件
+    YAML::Node root = YAML::LoadFile("/data/workspace/sylar/bin/conf/log.yml");
+    SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << root;
+
+    print_yaml(root, 0);
+}
+
 int main(int argc, char ** argv) {
     SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << g_int_value_config->getValue();
     SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << g_int_value_config->toString();
+
+    test_yaml();
     return 0;
 }
